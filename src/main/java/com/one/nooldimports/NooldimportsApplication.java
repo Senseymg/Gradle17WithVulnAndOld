3ada74c1e9d3411c01cd58e6913a458e8e5e4229
SenseyMG <33028581+Senseymg@users.noreply.github.com>
2024-12-19 09:35:03 +0300

    Add files via upload

---
+++ src/main/java/com/one/nooldimports/NooldimportsApplication.java
@@ -0,0 +1,13 @@
+package com.one.nooldimports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooldimportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooldimportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/nooldimports/NooldimportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.nooldimports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooldimportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
